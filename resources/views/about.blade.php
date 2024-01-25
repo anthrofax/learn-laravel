534c75b73dd21a83f19639df527091b658a6f643
anthrofax <afridhoikhsan@gmail.com>
2024-01-25 19:27:43 +0700

    Struktur Folder, Routes, & View

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Anthrofax | About</title>
+</head>
+<body>
+    <h1>About</h1>
+    <h3>Afridho Ikhsan</h3>
+    <p>afridhoikhsan@gmail.com</p>
+    <img src="img/myself.png" alt="Afridho" width="200">
+</body>
+</html>
