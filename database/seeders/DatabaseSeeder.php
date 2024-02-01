ad2608c5e72920cec3c411691ce4d6c474cd32e4
anthrofax <afridhoikhsan@gmail.com>
2024-02-01 21:19:51 +0700

    Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +21,70 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        User::create([
+            'name' => 'Afridho Ikhsan',
+            'email' => 'afridhoikhsan@gmail.com',
+            'password' => bcrypt(12345)
+        ]);
+        User::create([
+            'name' => 'Rizki Septiana',
+            'email' => 'rizki@gmail.com',
+            'password' => bcrypt(12345)
+        ]);
+        User::create([
+            'name' => 'Sopian Syauri',
+            'email' => 'sopian@gmail.com',
+            'password' => bcrypt(12345)
+        ]);
+
+        Category::create([
+            'name' => 'Programming',
+            'slug' => 'programming',
+        ]);
+        Category::create([
+            'name' => 'Web Design',
+            'slug' => 'web-design',
+        ]);
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal',
+        ]);
+
+        Post::create([
+            'title' => 'Master Python only in 70 Hours!',
+            'category_id' => '1',
+            'user_id' => '1',
+            'slug' => 'master-python-only-in-70-hours',
+            'excerpt' => 'fugiat quas laudantium iusto ipsa cum aliquam, accusantium maiores. Non ea tempore dolorem dicta at vero saepe, vitae earum nemo',
+            'author' => 'Afridho',
+            'body' => '<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti labore nesciunt nisi, beatae aut iusto quae aperiam atque dolorem nobis.</p><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam quae eaque inventore debitis illo unde neque impedit nulla maiores optio! Blanditiis culpa molestias perferendis nam quia ipsa iste ducimus eum!</p><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere blanditiis eveniet saepe neque quam maxime!</p>'
+        ]);
+        Post::create([
+            'title' => 'Learn Javascript From Beginners to Master',
+            'category_id' => '1',
+            'user_id' => '2',
+            'slug' => 'learn-javascript-from-beginners-to-master',
+            'excerpt' => 'fugiat quas laudantium iusto ipsa cum aliquam, accusantium maiores. Non ea tempore dolorem dicta at vero saepe, vitae earum nemo',
+            'author' => 'Rizki',
+            'body' => '<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti labore nesciunt nisi, beatae aut iusto quae aperiam atque dolorem nobis.</p><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam quae eaque inventore debitis illo unde neque impedit nulla maiores optio! Blanditiis culpa molestias perferendis nam quia ipsa iste ducimus eum!</p><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere blanditiis eveniet saepe neque quam maxime!</p>'
+        ]);
+        Post::create([
+            'title' => 'MVC Architecture',
+            'category_id' => '2',
+            'user_id' => '3',
+            'slug' => 'mvc-architecture',
+            'author' => 'Sopian',
+            'excerpt' => 'fugiat quas laudantium iusto ipsa cum aliquam, accusantium maiores. Non ea tempore dolorem dicta at vero saepe, vitae earum nemo',
+            'body' => '<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti labore nesciunt nisi, beatae aut iusto quae aperiam atque dolorem nobis.</p><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam quae eaque inventore debitis illo unde neque impedit nulla maiores optio! Blanditiis culpa molestias perferendis nam quia ipsa iste ducimus eum!</p><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere blanditiis eveniet saepe neque quam maxime!</p>'
+        ]);
+        Post::create([
+            'title' => 'Welcome to Samdalri',
+            'category_id' => '3',
+            'user_id' => '1',
+            'slug' => 'welcome-to-samdalri',
+            'author' => 'Afridho',
+            'excerpt' => 'fugiat quas laudantium iusto ipsa cum aliquam, accusantium maiores. Non ea tempore dolorem dicta at vero saepe, vitae earum nemo',
+            'body' => '<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti labore nesciunt nisi, beatae aut iusto quae aperiam atque dolorem nobis.</p><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam quae eaque inventore debitis illo unde neque impedit nulla maiores optio! Blanditiis culpa molestias perferendis nam quia ipsa iste ducimus eum!</p><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere blanditiis eveniet saepe neque quam maxime!</p>'
+        ]);
     }
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,13 +1,15 @@
 @extends('layouts.main')
 
 @section('container')
+    <h1 class="mb-4">Halaman Blog Post</h1>
     <?php foreach ($posts as $post) : ?>
-    <article>
-        <h2>
-            <a href="post/{{$post->slug }}"> {{$post->title}}</a>
-        </h2>
-        <h3>By:  {{$post->author }}</h3>
-        <p>{{ $post->excerpt}} </p>
+    <article class="mb-5 border-bottom p-4">
+        <h3>
+            <a href="post/{{ $post->slug }}"> {{ $post->title }}</a>
+        </h3>
+        <p >By <a class="text-decoration-none" href="">{{$post->author}}</a> in <a class="text-decoration-none" href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a></p>
+        <p>{{ $post->excerpt }} </p>
+        <a href="/post/{{$post->slug}}">Read more....</a>
     </article>
 
     <?php endforeach; ?>
