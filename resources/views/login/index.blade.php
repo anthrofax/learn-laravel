bebb9db25ee833f928d17291e7e9bf14220cd70f
anthrofax <afridhoikhsan@gmail.com>
2024-02-03 16:50:56 +0700

    User login & Middleware - Implement Login & Logout Feature & Named Route Concept

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -6,21 +6,39 @@
             <main class="form-signin w-100 m-auto">
                 <h1 class="h3 mb-3 fw-normal text-center">Please log in</h1>
 
-                @if(session()->has('success'))
-                <div class="alert alert-success alert-dismissible fade show" role="alert">
-                    {{session('success')}}
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
+                {{-- Tampilkan pesan saat pengguna berhasil mendaftarkan akunnya --}}
+                @if (session()->has('success'))
+                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                        {{ session('success') }}
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
                 @endif
 
-                <form>
+                {{-- Tampilkan pesan jika login gagal --}}
+                @if (session()->has('onError'))
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        {{ session('onError') }}
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                @endif
+
+                <form method="post">
+                    @csrf
+
                     <div class="form-floating">
-                        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                        <label for="floatingInput">Email address</label>
+                        <input type="email" class="form-control @error('email') is-invalid @enderror" name="email"
+                            id="email" placeholder="name@example.com" autofocus required value="{{old('email')}}">
+                        <label for="email">Email address</label>
+                        @error('email')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="form-floating">
-                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                        <label for="floatingPassword">Password</label>
+                        <input type="password" class="form-control" name="password" id="password" placeholder="Password"
+                            required>
+                        <label for="password">Password</label>
                     </div>
 
                     <button class="btn btn-primary w-100 py-2" type="submit">Log in</button>
