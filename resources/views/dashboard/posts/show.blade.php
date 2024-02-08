c8a4cc4217267abf7f2752a87124efe82812c3b4
anthrofax <afridhoikhsan@gmail.com>
2024-02-08 10:38:50 +0700

    #21 - Validate image file input & Display uploaded image on view

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -7,7 +7,8 @@
                 <h1 class="my-3">{{ $post->title }}</h1>
 
                 <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left"></span> Back to my post</a>
-                <a href="/dashboard/posts/{{$post->slug}}/edit" class="btn btn-warning"><span data-feather="edit"></span> Edit</a>
+                <a href="/dashboard/posts/{{ $post->slug }}/edit" class="btn btn-warning"><span data-feather="edit"></span>
+                    Edit</a>
                 <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
                     @method('delete')
                     @csrf
@@ -18,8 +19,15 @@
                     </button>
                 </form>
 
-                <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}"
-                    alt="{{ $post->category->name }}" class="img-fluid mt-3">
+                @if ($post->image)
+                    <div style="max-height: 350px; overflow: hidden">
+                        <img src={{ asset('storage/' . $post->image) }} alt="{{ $post->category->name }}"
+                            class="img-fluid mt-3">
+                    </div>
+                @else
+                    <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}"
+                        alt="{{ $post->category->name }}" class="img-fluid mt-3">
+                @endif
 
                 <article class="my-3 fs-5">
                     {!! $post->body !!}
